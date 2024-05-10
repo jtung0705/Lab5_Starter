dcf31f02f55945c6b412340b607b66ea848c5b7d
Jeffery Tung <jtung0705@gmail.com>
2024-05-10 05:38:15 -0700

    added stuff

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,50 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  var imag = document.querySelector("img");
+  var hornz = document.getElementById("horn-select");
+  hornz.addEventListener("change", function(){
+  if (document.getElementById("horn-select").value == 'air-horn' ){
+      imag.src = 'assets/images/air-horn.svg';
+  }
+  else if (document.getElementById("horn-select").value == 'car-horn'){
+      imag.src = 'assets/images/car-horn.svg';
+  }
+  else if (document.getElementById("horn-select").value == 'party-horn'){
+      imag.src = 'assets/images/party-horn.svg';
+    }
+  }
+)
+
+var volumeImage = document.querySelectorAll("img")[1];
+var cheekValue = document.getElementById("volume");
+cheekValue.addEventListener("change", function(){
+  if (cheekValue.value == 0 ){
+    volumeImage.src = 'assets/icons/volume-level-0.svg';
 }
+else if (cheekValue.value < 33 && cheekValue.value >= 1 ){
+  volumeImage.src = 'assets/icons/volume-level-1.svg';
+}
+else if (cheekValue.value < 67){
+  volumeImage.src = 'assets/icons/volume-level-2.svg';
+}
+else {
+  volumeImage.src = 'assets/icons/volume-level-3.svg';
+  }
+}
+)
+
+}
+ 
+  // TODO
+/*
+grab id value
+maybe grab image source?? change it each time the source thing in repo
+imag.addeventlistener wont work fors ome reason
+do i need to change the actual horn select
+ <option value="select" disabled selected>Select Horn:</option>
+        <option value="air-horn">Air Horn</option>
+        <option value="car-horn">Car Horn</option>
+        <option value="party-horn">Party Horn</option>
+horn-select
+*/
